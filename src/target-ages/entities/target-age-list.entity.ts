fbbffe226b5471716847a0ca90abe9207a8098c8
Yoshitaka Wada <wadslab@gmail.com>
2022-10-14 00:57:55 +0900

    remove target-age

--- src/target-ages/entities/target-age-list.entity.ts
+++
@@ -1,6 +0,0 @@
-import { ObjectType } from '@nestjs/graphql';
-import { PaginateResult } from 'src/common/models/paginate-result.model';
-import { TargetAge } from './target-age.entity';
-
-@ObjectType()
-export class TargetAgeList extends PaginateResult(TargetAge) {}
